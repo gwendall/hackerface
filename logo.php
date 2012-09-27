4db916be40bea26cd84431fd72e77a77aed22b22
Gwendall Esnault <gwendall.esnault@gmail.com>
2012-09-27 04:41:02 -0400

    ok

---
+++ logo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html itemscope itemtype="http://schema.org/Article">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link href="/index.css" type="text/css" rel="stylesheet">
+		<link href='http://fonts.googleapis.com/css?family=Dosis:800' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+		<link href="https://chrome.google.com/webstore/detail/ddiolncbbndlglfhigpabghoinceogjn" rel="chrome-webstore-item">
+		<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.1/themes/base/jquery-ui.css"/>
+		<title>hackerface</title>
+	</head>
+
+	<body>
+
+		<div class="logo">
+			<div class="title"><span style="color:#FF6600;">h</span>f</div>
+		</div>
+
+		
+	</body>
+
+</html>
